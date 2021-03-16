3a41d29517ef308d25ea1956b7cf6395886499de
Reeeesercher <liuyangly19@gmail.com>
2021-03-16 15:44:27 +0100

    init the demo

---
+++ src/main/java/com/erdnuss/springredisdockerdemo/SpringRedisDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.erdnuss.springredisdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erdnuss/springredisdockerdemo/SpringRedisDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdnuss.springredisdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
